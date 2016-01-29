714e6f6c989f13941e39c5448850c0c0ac757c89
FuRa1 <lalique.surf@gmail.ru>
2016-01-29 16:24:29 +0200

    fix route

--- app/js/page.js
+++ app/js/page.js
@@ -1,7 +1,7 @@
-/*$(function () {
+$(function () {
     $("ul li").click(function () {
         $("ul li").removeClass("active");
         $(this).addClass("active")
     });
     document.location.href = '#/repos';
-});*/
+});
